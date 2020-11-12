9646479fbecaaa51b61f2968755b63bed5c8f1c4
Danya <exemple@gmail.com>
2020-11-12 12:53:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// const res = confirm("You");
+// console.log(res);
+
+// const answer = prompt("You have?","18");
+// console.log(answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt("name",'');
+// answers[1] = prompt("first name",'');
+// answers[2] = prompt("last name",'');
+
+// console.log(typeof(answers));
+
+// const cat = ='toys';
+
+// console.log()
+
+// console.log(`arr`+`-obj`);
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
